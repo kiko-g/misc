16b822d49c653c3dd668a8fab9087866f2c74aa4
kiko-g <kikojpgoncalves@gmail.com>
2023-01-10 16:00:27 +0000

    add: twitch clip downloader

---
+++ python/main.py
@@ -0,0 +1,13 @@
+text = """
+wlqkfejf
+f   wflqwkfgw
+
+rrr                 rrr 
+fq
+egfqe
+rrrrrrrrr              3333333
+b rqtbeq     qerq     veqrre
+"""
+
+def textWithOnlySpaces(text):
+    return " ".join(text.split())
