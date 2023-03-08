ebc5142d52d7450e9cfcd5551548f824b9edf1ec
kiko-g <kikojpgoncalvesgmail.com>
2023-03-08 02:00:04 +0000

    add: afk improvements

--- python/afk.py
+++ python/afk.py
@@ -1,28 +1,32 @@
-import pyautogui as gui
-import random
 import time
+import random
+import pyautogui as gui
+from termcolor import colored
 
-print("Bot Running in background. Press ctrl + C to exit")
 
-gui.FAILSAFE = False
-
-try:
-    start_time = time.time()
-    time_interval = 5
+def move_mouse(start_time, interval_secs=10, move_duration=0.25):
     has_moved = True
-    
+    gui.FAILSAFE = False
+    print(colored(f"Bot Running in background. Press ctrl + C to exit", "blue"))
+
     while True:
         elapsed_time = time.time() - start_time
         print("\rElapsed time: {:.2f} seconds".format(elapsed_time), end="")
-        
-        if int(elapsed_time) % int(time_interval) == 0:
+
+        if int(elapsed_time) % int(interval_secs) == 0:
             has_moved = False
-            
+
         if not has_moved:
             x = random.randint(300, 500)
             y = random.randint(400, 600)
-            gui.moveTo(x, y, duration=1.0)
+            gui.moveTo(x, y, duration=move_duration)
             has_moved = True
 
-except KeyboardInterrupt:
-    print(f"\n\nExiting AFK. Lasted {time.time() - start_time} seconds")
+
+if __name__ == "__main__":
+    try:
+        start_time = time.time()
+        move_mouse(start_time)
+
+    except KeyboardInterrupt:
+        print(colored(f"\n\nExiting AFK. Lasted {time.time() - start_time} seconds", "red"))
