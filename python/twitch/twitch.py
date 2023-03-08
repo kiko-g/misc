c8499e2c2aa750c5ea22d78439b0dafc69fb453c
kiko-g <kikojpgoncalvesgmail.com>
2023-03-08 01:51:09 +0000

    twitch scraper updated

--- python/twitch/twitch.py
+++ python/twitch/twitch.py
@@ -1,22 +1,39 @@
+import os
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
-from twitch_clips import clip_ids
+from clips_db import clip_ids
+from termcolor import colored
 
 
-link_css_selector = "div.gap-4:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > div:nth-child(1) > li:nth-child(1) > div:nth-child(3) > a:nth-child(1)"
+def get_all_clips_download_urls():
+    urls = []
+    link_css_selector = "div.gap-4:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > div:nth-child(1) > li:nth-child(1) > div:nth-child(3) > a:nth-child(1)"
 
-# get all clips direct urls
-urls = []
-for clip_id in clip_ids:
-    response = requests.get(f"https://clipr.xyz/{clip_id}")
-    soup = BeautifulSoup(response.content, "html.parser")
-    link = soup.select(link_css_selector)[0]
-    url = link.get("href")
-    urls.append(url)
+    for i, clip_id in enumerate(clip_ids):
+        print(f"Getting clip download url ({i+1}/{len(clip_ids)})")
+        clip_url = f"https://clipr.xyz/{clip_id}"
+        response = requests.get(clip_url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        link = soup.select(link_css_selector)[0]
+        url = link.get("href")
+        urls.append(url)
 
-# download urls
-for url in urls:
+    return urls
+
+
+def download_clip(url):
     clipname = url.split("/")[-1]
     filename = f"data/{clipname}"
     urllib.request.urlretrieve(url, filename)
+
+
+if __name__ == "__main__":
+    urls = get_all_clips_download_urls()
+
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    for url in urls:
+        download_clip(url)
+        print(colored(f"Saved {url} to data/", "green"))
